6650bdf9a7597b345c93f184d4a21081f71754bf
Serhii Melnyk <serg.melnyk1998@gmail.com>
2023-09-28 04:26:13 +0300

    Customize admin panel for network & user

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from network.models import Profile, Post, Like
+
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Like)
